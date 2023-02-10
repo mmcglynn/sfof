ff7331c74b6c26b43124f2fc762bd0c3d1e37e5f
Michael McGlynn <mike@reignition.net>
2023-02-10 06:52:07 -0500

    Spacing

--- single-state.php
+++ single-state.php
@@ -16,26 +16,26 @@ get_header();
 	</div>
 	<main id="primary" class="site-main padded-top-large padded-bottom-large">
 		<div class="inner">
-			<?php
-			while ( have_posts() ) :
-				the_post();
+            <?php
+            while ( have_posts() ) :
+                the_post();
 
-				get_template_part( 'template-parts/content', get_post_type() );
+                get_template_part( 'template-parts/content', get_post_type() );
 
-				the_post_navigation(
-					array(
-						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sfof' ) . '</span> <span class="nav-title">%title</span>',
-						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sfof' ) . '</span> <span class="nav-title">%title</span>',
-					)
-				);
+                the_post_navigation(
+                    array(
+                        'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sfof' ) . '</span> <span class="nav-title">%title</span>',
+                        'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sfof' ) . '</span> <span class="nav-title">%title</span>',
+                    )
+                );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
 
-			endwhile; // End of the loop.
-			?>
+            endwhile; // End of the loop.
+            ?>
 		</div>
 		</div>
 	</main><!-- #main -->
--- template-parts/content-state.php
+++ template-parts/content-state.php
@@ -10,59 +10,59 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		if (is_front_page() == false) {
-	?>
-	<div class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</div><!-- .entry-header -->
-	<?php
-		}
-	?>
+    <?php
+    if (is_front_page() == false) {
+        ?>
+		<div class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div><!-- .entry-header -->
+        <?php
+    }
+    ?>
 	<div class="flex-box has-sidebar space-between">
 		<div class="entry-content">
-			<?php
-			the_content();
+            <?php
+            the_content();
 
-			/*wp_link_pages(
-				array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sfof' ),
-					'after'  => '</div>',
-				)
-			);*/
-			
-			if ( get_edit_post_link() ) : ?>
+            /*wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sfof' ),
+                    'after'  => '</div>',
+                )
+            );*/
+
+            if ( get_edit_post_link() ) : ?>
 				<footer class="entry-footer">
-					<?php
-					edit_post_link(
-						sprintf(
-							wp_kses(
-								/* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Edit <span class="screen-reader-text">%s</span>', 'sfof' ),
-								array(
-									'span' => array(
-										'class' => array(),
-									),
-								)
-							),
-							wp_kses_post( get_the_title() )
-						),
-						'<span class="edit-link">',
-						'</span>'
-					);
-					?>
+                    <?php
+                    edit_post_link(
+                        sprintf(
+                            wp_kses(
+                            /* translators: %s: Name of current post. Only visible to screen readers */
+                                __( 'Edit <span class="screen-reader-text">%s</span>', 'sfof' ),
+                                array(
+                                    'span' => array(
+                                        'class' => array(),
+                                    ),
+                                )
+                            ),
+                            wp_kses_post( get_the_title() )
+                        ),
+                        '<span class="edit-link">',
+                        '</span>'
+                    );
+                    ?>
 				</footer><!-- .entry-footer -->
-			<?php endif; ?>
+            <?php endif; ?>
 		</div><!-- .entry-content -->
 		<aside class="sidebar">
-		   <?php
-		   if ( has_post_thumbnail() ) {
-			   /* Member Thumbnail */
-			   echo '<div class="state-image full-width margin-bottom-large">';
-				   echo '<img src="'.get_the_post_thumbnail_url(get_the_ID(),'large').'" class="full-width" />';
-			   echo '</div>';
-		   }
-		   ?>
+            <?php
+            if ( has_post_thumbnail() ) {
+                /* Member Thumbnail */
+                echo '<div class="state-image full-width margin-bottom-large">';
+                echo '<img src="'.get_the_post_thumbnail_url(get_the_ID(),'large').'" class="full-width" />';
+                echo '</div>';
+            }
+            ?>
 		</aside>
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
